8323759fa8ffa3e701a9fb123389427f1601916e
v_bonchevski <v.bonchevski@valteodigital.com>
2019-08-28 14:38:24 +0300

    Updates

---
+++ precache-manifest.a9e946380b1c2a590c44921fe428a07e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c21c7e284481abb65efb3aca98e00c",
+    "url": "/github.io/Basics-App/index.html"
+  },
+  {
+    "revision": "80aa7ea1eb68fc1059cf",
+    "url": "/github.io/Basics-App/static/css/main.ce632bc1.chunk.css"
+  },
+  {
+    "revision": "c6097d53ed50abbb7041",
+    "url": "/github.io/Basics-App/static/js/2.1817496a.chunk.js"
+  },
+  {
+    "revision": "80aa7ea1eb68fc1059cf",
+    "url": "/github.io/Basics-App/static/js/main.9f2ef00f.chunk.js"
+  },
+  {
+    "revision": "28d7ac7608e234561e16",
+    "url": "/github.io/Basics-App/static/js/runtime~main.d6ffdb38.js"
+  }
+]);
